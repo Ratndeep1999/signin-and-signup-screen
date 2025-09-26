0c88481ccf5ab9cf12af2f514624ffde6c5a4aa6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-26 15:53:02 +0530

    nullable suffix icon add

--- lib/Custom Widgets/custom_textfield_widget.dart
+++ lib/Custom Widgets/custom_textfield_widget.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 
 class CustomTextfieldWidget extends StatelessWidget {
   const CustomTextfieldWidget({
-    super.key, required this.hintText, this.suffixIcon,
+    super.key,
+    required this.hintText,
+    this.suffixIcon,
+    required this.obscureText,
   });
 
   // Attributes
   final String hintText;
   final IconData? suffixIcon;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
+      obscureText: obscureText,
+      obscuringCharacter: '*',
       decoration: InputDecoration(
         // hintText
         hintText: hintText,
@@ -21,20 +27,13 @@ class CustomTextfieldWidget extends StatelessWidget {
           fontWeight: FontWeight.w500,
         ),
         // padding of hintText
-        contentPadding: EdgeInsets.symmetric(
-          horizontal: 28.0,
-          vertical: 16.0,
-        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 16.0),
         // suffixIcon
         suffixIcon: Icon(suffixIcon),
         suffixIconColor: Colors.black54,
         // min right padding of icon
-        suffixIconConstraints: BoxConstraints(
-          minWidth: 80.0,
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(50.0),
-        ),
+        suffixIconConstraints: BoxConstraints(minWidth: 80.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
       ),
     );
   }
