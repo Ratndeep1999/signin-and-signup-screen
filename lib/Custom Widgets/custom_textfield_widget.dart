f877d544375176652be85d81ae3ec5c7f8b98638
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-26 15:38:33 +0530

    textfield refactor

--- lib/Custom Widgets/custom_textfield_widget.dart
+++ lib/Custom Widgets/custom_textfield_widget.dart
@@ -2,25 +2,33 @@ import 'package:flutter/material.dart';
 
 class CustomTextfieldWidget extends StatelessWidget {
   const CustomTextfieldWidget({
-    super.key,
+    super.key, required this.hintText, this.suffixIcon,
   });
+
+  // Attributes
+  final String hintText;
+  final IconData? suffixIcon;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        hintText: "Your Email Address",
+        // hintText
+        hintText: hintText,
         hintStyle: TextStyle(
           color: Colors.black45,
           fontWeight: FontWeight.w500,
         ),
+        // padding of hintText
         contentPadding: EdgeInsets.symmetric(
           horizontal: 28.0,
           vertical: 16.0,
         ),
-        suffixIcon: Icon(Icons.person),
+        // suffixIcon
+        suffixIcon: Icon(suffixIcon),
         suffixIconColor: Colors.black54,
+        // min right padding of icon
         suffixIconConstraints: BoxConstraints(
           minWidth: 80.0,
         ),
