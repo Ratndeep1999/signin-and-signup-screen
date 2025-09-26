a045bde6c168e933a38f32644110179b44728144
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-26 15:21:37 +0530

    field label refactor

--- lib/Custom Widgets/custom_label_widget.dart
+++ lib/Custom Widgets/custom_label_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CustomFieldLabels extends StatelessWidget {
-  const CustomFieldLabels({super.key, required this.label});
+class CustomLabelWidget extends StatelessWidget {
+  const CustomLabelWidget({super.key, required this.label});
 
   final String label;
 
