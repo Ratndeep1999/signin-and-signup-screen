c956cb312e73248ff4185f25c33606cb177ba353
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-26 14:40:27 +0530

    background design add

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -28,13 +28,34 @@ class SigninScreen extends StatelessWidget {
   }
 }
 
+/// Clipped background Design
 class BackgroundDesign extends CustomClipper<Path> {
   @override
-  Path getClip(Size size) {}
+  Path getClip(Size size) {
+    //
+    Path path = Path();
+
+    // extract height and width
+    final width = size.width;
+    final height = size.height;
+
+    // First line
+    path.lineTo(0, height - 350);
+
+    // First curve
+    path.quadraticBezierTo(5, height - 230, 250, height - 100);
+
+    // Second curve
+    path.quadraticBezierTo(350, height - 100, width, height - 140);
+
+    // Final line
+    path.lineTo(width, 0);
+
+    return path;
+  }
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    throw UnimplementedError();
+    return false;
   }
 }
