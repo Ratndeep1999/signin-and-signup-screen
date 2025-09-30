7ffc40695cd532b3d75b12965119eef226bef69d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-30 14:20:03 +0530

    call back function add

--- lib/Custom Widgets/custom_button.dart
+++ lib/Custom Widgets/custom_button.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.label});
+  const CustomButton(
+      {super.key, required this.label, required this.loginClick});
 
   final String label;
+  final VoidCallback loginClick;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: loginClick,
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.orange.shade300,
           foregroundColor: Colors.black,
