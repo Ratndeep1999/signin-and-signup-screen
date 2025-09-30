32e1430cb9d48a76c950c9fbc406059114223ef9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-30 13:48:25 +0530

    validation add for email address and password.

--- lib/signin_screen.dart
+++ lib/signin_screen.dart
@@ -131,6 +131,32 @@ class _SigninScreenState extends State<SigninScreen> {
                                   });
                                 },
                                 textController: _password,
+                                validation: (String? password) {
+                                  if (password == null || password.isEmpty) {
+                                    return "Please enter your password";
+                                  }
+                                  if (password.length > 8) {
+                                    return "Password must be at least 8 characters";
+                                  }
+                                  if (password.contains(' ')) {
+                                    return "Space is not allowed";
+                                  }
+                                  if (!RegExp(r'[A-Z]').hasMatch(password)) {
+                                    return "Password must contain at least one uppercase letter";
+                                  }
+                                  if (!RegExp(r'[a-z]').hasMatch(password)) {
+                                    return "Password must contain at least one lowercase letter";
+                                  }
+                                  if (!RegExp(r'[0-9]').hasMatch(password)) {
+                                    return "Password must contain at least one number";
+                                  }
+                                  if (!RegExp(
+                                    r'[!@\$&*~_]',
+                                  ).hasMatch(password)) {
+                                    return "Password must contain at least one special character (!@#\$&*~_)";
+                                  }
+                                  return null;
+                                },
                               ),
                               const SizedBox(height: 24.0),
 
