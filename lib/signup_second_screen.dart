dfae210bc0ff5db1ae5ce9af03d0b4e041363c21
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-10-01 14:13:40 +0530

    signup second screen add

---
+++ lib/signup_second_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignupSecondScreen extends StatelessWidget {
+  const SignupSecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      /// Body
+      body: SafeArea(child: Stack(children: [],)),
+    );
+  }
+}
