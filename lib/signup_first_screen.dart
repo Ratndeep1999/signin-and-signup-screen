afa6c8c84a510a7f68fa71878a60dab81858f0e6
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-10-01 14:03:21 +0530

    padding increase

--- lib/signup_first_screen.dart
+++ lib/signup_first_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:signin_and_signup_screens/Custom%20Widgets/custom_text_field.dart';
-
 import 'Custom Widgets/customClippingDesign.dart';
 import 'Custom Widgets/custom_button.dart';
 import 'Custom Widgets/custom_clickable_text.dart';
@@ -18,6 +17,7 @@ class SignupFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       /// Appbar
       appBar: AppBar(backgroundColor: Colors.orange.shade300),
 
@@ -31,6 +31,7 @@ class SignupFirstScreen extends StatelessWidget {
           child: SafeArea(
             child: Stack(
               children: [
+
                 /// Background Custom Design
                 const CustomClippingDesign(),
 
@@ -38,6 +39,7 @@ class SignupFirstScreen extends StatelessWidget {
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+
                     /// Heading Text
                     CustomHeading(
                       bigText: "Join Us\n",
@@ -50,6 +52,7 @@ class SignupFirstScreen extends StatelessWidget {
                       margin: EdgeInsets.symmetric(horizontal: 20.0),
                       elevation: 5.0,
                       child: Padding(
+
                         /// Content Padding
                         padding: const EdgeInsets.symmetric(
                           horizontal: 20.0,
@@ -57,11 +60,12 @@ class SignupFirstScreen extends StatelessWidget {
                         ),
                         child: Column(
                           children: [
+
                             /// 2nd Heading
                             CustomSecondHeading(
                               bigText: "Personal Info\n",
                               smallText:
-                                  "Lorem ipsum dolor sit amet. consectetuer adipiscing sed\n diam nonummy nibh euismod tincidunt.",
+                              "Lorem ipsum dolor sit amet. consectetuer adipiscing sed\n diam nonummy nibh euismod tincidunt.",
                             ),
                             const SizedBox(height: 32.0),
 
