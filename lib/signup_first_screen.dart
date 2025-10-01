08604632ddf66d5fd92238bc994bb4859d06a51d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-10-01 14:09:11 +0530

    form key add

--- lib/signup_first_screen.dart
+++ lib/signup_first_screen.dart
@@ -10,6 +10,7 @@ import 'Custom Widgets/custom_text_field_label.dart';
 class SignupFirstScreen extends StatelessWidget {
   SignupFirstScreen({super.key});
 
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _firstName = TextEditingController();
   final TextEditingController _lastName = TextEditingController();
   final TextEditingController _emailAddress = TextEditingController();
@@ -17,7 +18,6 @@ class SignupFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       /// Appbar
       appBar: AppBar(backgroundColor: Colors.orange.shade300),
 
@@ -31,7 +31,6 @@ class SignupFirstScreen extends StatelessWidget {
           child: SafeArea(
             child: Stack(
               children: [
-
                 /// Background Custom Design
                 const CustomClippingDesign(),
 
@@ -39,7 +38,6 @@ class SignupFirstScreen extends StatelessWidget {
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-
                     /// Heading Text
                     CustomHeading(
                       bigText: "Join Us\n",
@@ -52,117 +50,120 @@ class SignupFirstScreen extends StatelessWidget {
                       margin: EdgeInsets.symmetric(horizontal: 20.0),
                       elevation: 5.0,
                       child: Padding(
-
                         /// Content Padding
                         padding: const EdgeInsets.symmetric(
                           horizontal: 20.0,
                           vertical: 32.0,
                         ),
-                        child: Column(
-                          children: [
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              /// 2nd Heading
+                              CustomSecondHeading(
+                                bigText: "Personal Info\n",
+                                smallText:
+                                    "Lorem ipsum dolor sit amet. consectetuer adipiscing sed\n diam nonummy nibh euismod tincidunt.",
+                              ),
+                              const SizedBox(height: 32.0),
 
-                            /// 2nd Heading
-                            CustomSecondHeading(
-                              bigText: "Personal Info\n",
-                              smallText:
-                              "Lorem ipsum dolor sit amet. consectetuer adipiscing sed\n diam nonummy nibh euismod tincidunt.",
-                            ),
-                            const SizedBox(height: 32.0),
+                              /// Your Name Label
+                              const CustomTextFieldLabel(label: 'Your Name'),
+                              const SizedBox(height: 8.0),
 
-                            /// Your Name Label
-                            const CustomTextFieldLabel(label: 'Your Name'),
-                            const SizedBox(height: 8.0),
-
-                            /// First and Last Name
-                            Row(
-                              //mainAxisSize: MainAxisSize.min,
-                              children: [
-                                // First Name
-                                Flexible(
-                                  child: CustomTextField(
-                                    hintText: 'First Name',
-                                    keyboardType: TextInputType.name,
-                                    obscureText: false,
-                                    dynamicSuffixIcon: false,
-                                    textController: _firstName,
-                                    validation: (String? firstName) {
-                                      if (firstName == null ||
-                                          firstName.isEmpty) {
-                                        return 'Please enter your first name';
-                                      }
-                                      return null;
-                                    },
+                              /// First and Last Name
+                              Row(
+                                //mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  // First Name
+                                  Flexible(
+                                    child: CustomTextField(
+                                      hintText: 'First Name',
+                                      keyboardType: TextInputType.name,
+                                      obscureText: false,
+                                      dynamicSuffixIcon: false,
+                                      textController: _firstName,
+                                      validation: (String? firstName) {
+                                        if (firstName == null ||
+                                            firstName.isEmpty) {
+                                          return 'Please enter your first name';
+                                        }
+                                        return null;
+                                      },
+                                    ),
                                   ),
-                                ),
 
-                                SizedBox(width: 10.0),
+                                  SizedBox(width: 10.0),
 
-                                // Last Name
-                                Flexible(
-                                  child: CustomTextField(
-                                    hintText: 'Last Name',
-                                    keyboardType: TextInputType.name,
-                                    obscureText: false,
-                                    dynamicSuffixIcon: false,
-                                    textController: _lastName,
-                                    validation: (String? lastName) {
-                                      if (lastName == null ||
-                                          lastName.isEmpty) {
-                                        return 'Please enter your first name';
-                                      }
-                                      return null;
-                                    },
+                                  // Last Name
+                                  Flexible(
+                                    child: CustomTextField(
+                                      hintText: 'Last Name',
+                                      keyboardType: TextInputType.name,
+                                      obscureText: false,
+                                      dynamicSuffixIcon: false,
+                                      textController: _lastName,
+                                      validation: (String? lastName) {
+                                        if (lastName == null ||
+                                            lastName.isEmpty) {
+                                          return 'Please enter your first name';
+                                        }
+                                        return null;
+                                      },
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
+                                ],
+                              ),
 
-                            const SizedBox(height: 24.0),
+                              const SizedBox(height: 24.0),
 
-                            /// Email Label
-                            const CustomTextFieldLabel(label: 'Email Address'),
-                            const SizedBox(height: 8.0),
+                              /// Email Label
+                              const CustomTextFieldLabel(
+                                label: 'Email Address',
+                              ),
+                              const SizedBox(height: 8.0),
 
-                            /// Email Address Field
-                            CustomTextField(
-                              hintText: "Your Email Address",
-                              keyboardType: TextInputType.emailAddress,
-                              suffixIcon: Icons.email,
-                              obscureText: false,
-                              dynamicSuffixIcon: false,
-                              textController: _emailAddress,
-                              validation: _emailValidation,
-                            ),
-                            const SizedBox(height: 24.0),
+                              /// Email Address Field
+                              CustomTextField(
+                                hintText: "Your Email Address",
+                                keyboardType: TextInputType.emailAddress,
+                                suffixIcon: Icons.email,
+                                obscureText: false,
+                                dynamicSuffixIcon: false,
+                                textController: _emailAddress,
+                                validation: _emailValidation,
+                              ),
+                              const SizedBox(height: 24.0),
 
-                            /// Username Label
-                            const CustomTextFieldLabel(label: 'Username'),
-                            const SizedBox(height: 8.0),
+                              /// Username Label
+                              const CustomTextFieldLabel(label: 'Username'),
+                              const SizedBox(height: 8.0),
 
-                            /// Username Field
-                            CustomTextField(
-                              hintText: "example1234",
-                              keyboardType: TextInputType.emailAddress,
-                              suffixIcon: Icons.email,
-                              obscureText: false,
-                              dynamicSuffixIcon: false,
-                              textController: _emailAddress,
-                              validation: _emailValidation,
-                            ),
-                            const SizedBox(height: 24.0),
+                              /// Username Field
+                              CustomTextField(
+                                hintText: "example1234",
+                                keyboardType: TextInputType.emailAddress,
+                                suffixIcon: Icons.email,
+                                obscureText: false,
+                                dynamicSuffixIcon: false,
+                                textController: _emailAddress,
+                                validation: _emailValidation,
+                              ),
+                              const SizedBox(height: 24.0),
 
-                            /// Save & Continue Button
-                            CustomButton(
-                              label: 'Save & Continue',
-                              loginClick: () {
-                                /// If Details is valid then Login..
-                              },
-                            ),
-                            const SizedBox(height: 25.0),
+                              /// Save & Continue Button
+                              CustomButton(
+                                label: 'Save & Continue',
+                                loginClick: () {
+                                  /// If Details is valid then Login..
+                                },
+                              ),
+                              const SizedBox(height: 25.0),
 
-                            /// Create Account text Button
-                            const CustomClickableText(label: 'Back to Login'),
-                          ],
+                              /// Create Account text Button
+                              const CustomClickableText(label: 'Back to Login'),
+                            ],
+                          ),
                         ),
                       ),
                     ),
