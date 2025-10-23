0c2a9b8b4f62cc971bcbe4c96b42eed86bb1e013
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-23 19:26:09 +0530

    give controller properly

---
+++ lib/Custom Widgets/custom_phone_number_field.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:intl_phone_field/phone_number.dart';
+
+class CustomPhoneNumberField extends StatelessWidget {
+  const CustomPhoneNumberField({
+    super.key,
+    required this.onChanged,
+    required this.suffixIcon,
+    required this.suffixIconColor,
+    required this.validation,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+  final ValueChanged<PhoneNumber>? onChanged;
+  final FutureOr<String?> Function(PhoneNumber?)? validation;
+  final IconData suffixIcon;
+  final Color suffixIconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntlPhoneField(
+      controller: controller,
+      keyboardType: TextInputType.phone,
+      initialCountryCode: 'IN',
+      onChanged: onChanged,
+      //onSaved: ,
+      validator: validation,
+      // dropdown
+      dropdownIconPosition: IconPosition.trailing,
+      dropdownIcon: Icon(
+        Icons.keyboard_arrow_down_rounded,
+        size: 20,
+        color: Colors.black54,
+      ),
+      dropdownTextStyle: TextStyle(
+        fontSize: 14.0,
+        color: Colors.black54,
+        fontWeight: FontWeight.w500,
+      ),
+      disableLengthCheck: true,
+      // input phone number style
+      style: TextStyle(
+        fontSize: 14.0,
+        color: Colors.black54,
+        fontWeight: FontWeight.w500,
+      ),
+      decoration: InputDecoration(
+        // hint text
+        hint: Text(
+          "1234 5678 9101",
+          style: TextStyle(
+            fontSize: 14.0,
+            color: Colors.black54,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        errorStyle: TextStyle(
+          color: Color(0xFFefb744),
+          fontSize: 10.0,
+          fontWeight: FontWeight.bold,
+        ),
+        // padding of hintText
+        contentPadding: EdgeInsets.only(top: 12.0, bottom: 12.0, left: 20.0),
+        suffixIcon: Icon(suffixIcon, size: 18),
+        suffixIconColor: suffixIconColor,
+        suffixIconConstraints: BoxConstraints(minWidth: 80.0),
+        // borders
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(50.0)),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: BorderSide(
+            color: Colors.black12,
+            width: 1.0,
+          ), // default border color
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: BorderSide(
+            color: Colors.black12,
+            width: 2.0,
+          ), // active border color
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: BorderSide(
+            color: Color(0xFFefb744),
+            width: 2.0,
+          ), // error state
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(50.0),
+          borderSide: BorderSide(color: Color(0xFFefb744), width: 2.0),
+        ),
+      ),
+    );
+  }
+}
