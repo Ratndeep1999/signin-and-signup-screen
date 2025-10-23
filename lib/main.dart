d5a738a91489db0bdc8450af9ed7f10674f6d7e6
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-23 10:08:16 +0530

    splash screen navigation add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:signin_and_signup_screens/signin_screen.dart';
+import 'package:signin_and_signup_screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SigningScreen(),
+      home: SplashScreen(),
     );
   }
 }
