4da45151e541fbe870b6aeb347242eac23bd01a7
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-23 09:17:52 +0530

    dashboard icon add

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'Custom Widgets/custom_clipping_design.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Column()),
+      appBar: AppBar(backgroundColor: Color(0xFFefb744)),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            /// Background Custom Design
+            const CustomClippingDesign(),
+
+            /// Dashboard Icon
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                "assets/icons/home.png",
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
