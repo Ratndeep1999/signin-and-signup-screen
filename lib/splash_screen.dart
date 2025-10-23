57b580d9c1eea1418957d76e871f55039b0187df
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-23 10:23:32 +0530

    splash screen icon add

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,9 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:signin_and_signup_screens/signin_screen.dart';
 
 import 'Custom Widgets/custom_clipping_design.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 2000), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SigningScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +32,15 @@ class SplashScreen extends StatelessWidget {
           children: [
             /// Background Custom Design
             const CustomClippingDesign(),
+
+            /// Splash Icon
+            Image.asset(
+              "assets/icons/splash.png",
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+
           ],
         ),
       ),
