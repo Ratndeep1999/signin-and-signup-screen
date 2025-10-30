c80cf554945840515c927b2c6347a30621d3349b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-30 10:06:33 +0530

    Forgot password section: enter email screen add

---
+++ lib/Forgot Password Section/enter_email_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EnterEmailScreen extends StatelessWidget {
+  const EnterEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: child),
+    );
+  }
+}
