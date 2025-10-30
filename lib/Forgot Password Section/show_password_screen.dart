66247f6bdb179d462ea02205b3a36d656df66c58
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-10-30 14:23:28 +0530

    show password screen add

---
+++ lib/Forgot Password Section/show_password_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:signin_and_signup_screens/Custom%20Widgets/custom_clipping_design.dart';
+
+class ShowPasswordScreen extends StatelessWidget {
+  const ShowPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFefb744),
+        automaticallyImplyLeading: false,
+      ),
+      body: SafeArea(child: Stack(
+
+      )),
+    );
+  }
+}
