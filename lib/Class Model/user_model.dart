d5e68f35b1fa9f99492d3ee752717f5a72380bc4
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-13 18:15:32 +0530

    method add to convert map to model to read data

--- lib/Class Model/user_model.dart
+++ lib/Class Model/user_model.dart
@@ -1,5 +1,4 @@
 class UserModel {
-
   /// Parameters
   final int? id;
   final String firstName;
@@ -27,18 +26,34 @@ class UserModel {
   });
 
   /// Method to convert object to map (use to inserting data)
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
-      'id' : id,
-      'firstName' : firstName,
-      'lastName' : lastName,
-      'emailId' : emailId,
-      'userName' : userName,
-      'birthday' : birthday,
-      'password' : password,
-      'phoneNumber' : phoneNumber,
-      'securityQuestion' : securityQuestion,
-      'securityAnswer' : securityAnswer,
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'emailId': emailId,
+      'userName': userName,
+      'birthday': birthday,
+      'password': password,
+      'phoneNumber': phoneNumber,
+      'securityQuestion': securityQuestion,
+      'securityAnswer': securityAnswer,
     };
+  }
+
+  /// Method to convert map to object (use to read data)
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      emailId: map['emailId'],
+      userName: map['userName'],
+      birthday: map['birthday'],
+      password: map['password'],
+      phoneNumber: map['phoneNumber'],
+      securityQuestion: map['securityQuestion'],
+      securityAnswer: map['securityAnswer'],
+    );
   }
 }
