b18383879c791670b7db2f224a54614c08e48faa
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-13 20:03:38 +0530

    to create table in database method add

--- lib/Database/db_table.dart
+++ lib/Database/db_table.dart
@@ -1,6 +1,45 @@
+import 'package:sqflite/sqflite.dart';
+
 class DBTable {
 
-  createTable(Database database){
-    final
+  // table name
+  final String tableName = 'user';
+  // column names
+  final String id = 'id';
+  final String firstName = 'First_Name';
+  final String lastName = 'Last_Name';
+  final String emailId = 'Email_Id';
+  final String userName = 'User_Name';
+  final String birthday = 'Birthday';
+  final String password = 'Password';
+  final String phoneNo = 'Phone_No';
+  final String secuQue = 'Security_Que';
+  final String secuAns = 'Security_Ans';
+
+  /// Method that create table in database
+  Future<void> createTable(Database database) async {
+    await database.execute(
+      '''
+      CREATE TABLE IF NOT EXISTS $tableName (
+      "$id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      "$firstName" TEXT NOT NULL,
+      "$lastName" TEXT NOT NULL,
+      "$emailId" TEXT NOT NULL,
+      "$userName" TEXT NOT NULL,
+      "$birthday" TEXT NOT NULL,
+      "$password" TEXT NOT NULL,
+      "$phoneNo" TEXT NOT NULL,
+      "$secuQue" TEXT NOT NULL,
+      "$secuAns" TEXT NOT NULL,
+      )
+      '''
+    );
   }
+
+  /// Method to insert user data
+
+
+
+
+
 }
